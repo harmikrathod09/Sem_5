f75e287eddd6464d9b8b343700a4fffb2a60d874
harmik0919 <harmikrathod56@gmail.com>
2025-07-01 22:38:02 +0530

    1 july

--- DAA/Lab4/heap_sort.c
+++ DAA/Lab4/heap_sort.c
@@ -4,15 +4,24 @@
 
 void max_heapify(int arr[], int n, int i)
 {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    int temp;
+    int largest = i; 
+    int left = 2 * i + 1; 
+    int right = 2 * i + 2; 
 
-    // if (left < n && arr[left] > arr[largest])
-        
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
 
-   
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        max_heapify(arr, n, largest);
+    }
 }
 
 void build_max_heap(int arr[], int n)
@@ -23,10 +32,74 @@ void build_max_heap(int arr[], int n)
     }
 }
 
-void heapSort(int arr[],int n){
-    build_max_heap(arr,n);
+void heapSort(int arr[], int n)
+{
+    build_max_heap(arr, n);
+
+    for (int i = n - 1; i >= 1; i--)
+    {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        max_heapify(arr, i, 0);
+    }
 }
 
-void heapSort(){
+int readFile(const char *filename, int arr[], int n)
+{
+    FILE *f = fopen(filename, "r");
+    if (!f)
+    {
+        printf("File not found\n");
+        return 0;
+    }
 
+    for (int i = 0; i < n; i++)
+        fscanf(f, "%d", &arr[i]);
+
+    fclose(f);
+    return 1;
+}
+
+void printArray(int arr[])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+void main()
+{
+  int arr[N];
+    clock_t start, end;
+    double time_taken;
+    if (readFile("../Arrays/best_case_100.txt", arr, N))
+    {
+        start = clock();
+        heapSort(arr, N);
+        end = clock();
+        time_taken = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
+        printf("Best case of Element %d is %f\n", N, time_taken);
+    }
+
+    if (readFile("../Arrays/average_case_100.txt", arr, N))
+    {
+        start = clock();
+        heapSort(arr, N);
+        end = clock();
+        time_taken = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
+        printf("Average case of Element %d is %f\n", N, time_taken);
+    }
+
+    if (readFile("../Arrays/worst_case_100.txt", arr, N))
+    {
+        start = clock();
+        heapSort(arr, N);
+        end = clock();
+        time_taken = ((double)(end - start)) / CLOCKS_PER_SEC * 1000;
+        printf("Worst case of Element %d is %f\n", N, time_taken);
+    }
 }
